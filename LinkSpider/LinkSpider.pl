80d88a85a89b7f957e1a74f910229cae2cfc3424
earata15 <earata11@gmail.com>
2017-07-30 09:55:51 -0400

    Update LinkSpider.pl
    add wget call and create file with HTML for each search

--- LinkSpider/LinkSpider.pl
+++ LinkSpider/LinkSpider.pl
@@ -3,43 +3,66 @@
 # rough sketch
 
 use Term::Prompt;
-# this is first run skeleton that takes user input for search terms and creates an array
-# to do : create URL's for search pages to visit, scrape HTML, curate a list of URL's of campaign pages
 
-
-print "\nEnter as many search terms as you like. \nType \"done\" when input is done.\n\n";
-# Here is where we get the code from the user:
-
-@searchTerms = ();
-$code = prompt('x', 'Search term: ', '', '');
-
-while($code !~ /^done$/){
-	push(@searchTerms,$code);
-	$code = prompt('x', 'Search term: ', '', '');
-} 
-
-print "\nAn array has been formed with the following search terms: \n";
-foreach $term (@searchTerms){
-	# regex replace spaces in search term with % or whatever character that was
-	#my $url = "www.gofundme.com" . "......." . $term;
-	#searchUrl();
-	print "\n" . $term;
-}
-print "\n";
-
-
+$urlfile = "";
 
 sub searchUrl
 {
 	# grab HTML from search results page and create a list of URL's of pages for us to go through
-
+	my $url = shift;
 	# pasted in wget command from Snoopy.pl / LinkCheck.pl... will change this to include our URL and otherwise suit our needs
-    #my $cmd = "wget -q -O $urlfile $url";
+    my $cmd = "wget -q -O $urlfile $url";
     
     # Print the command for debugging.
-    # print "running $cmd\n";
+    print "running $cmd\n\n";
     
     # We use the back tics to run the command.
-    #`$cmd`;
+    `$cmd`;
 
+    if($?)
+    {
+        # We got some kind of error. Assume its 404.
+        print OUTFILE "Dead Link: $url\n";
+    }
+    else
+    {
+        # Link is good. Open the file or stop the program if we can not.
+        open URLFILE, "< $urlfile" or die "Could not open $urlfile: $!";
+
+        # to-do: search result info in the HTML, probably using HTML::TreeBuilder to parse the HTML into a tree
+        # (HTML is saved in $urlfile)
+        # looking for a div tag with class="js-fund-tile", and information nested within that tag
+        # run URL against basic tests
+        # if pass, pull information and format into an excel sheet - name of campaign, URL, location, funding goal
+
+	}
 }
+
+sub main
+{
+	mkdir "temp/";
+	print "\nEnter as many search terms as you like. \nType \"done\" when input is done.\n\n";
+	# Here is where we get the code from the user:
+
+	@searchTerms = ();
+	$code = prompt('x', 'Search term: ', '', '');
+
+	while($code !~ /^done$/){
+		push(@searchTerms,$code);
+		$code = prompt('x', 'Search term: ', '', '');
+	} 
+
+	my $count = 1;
+	foreach $term (@searchTerms){
+		$term =~ s/\s/\%/g;
+		my $url = "https://www.gofundme.com/mvc.php?route=category&term=" . $term . "&country=US";
+		my $url2 = $url . "&page=2";
+		print "URL generated: $url\n";
+		$urlfile = "temp/wgetGot-" . $count . ".html";
+		searchUrl($url);
+		searchUrl($url2);
+		$count++;
+	}
+}
+
+main();
